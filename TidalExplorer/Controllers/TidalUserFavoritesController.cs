9ecbdbe418bd493c3db99a9b3f169ede866797e7
Henning Klokkeråsen <henning.klokkeraasen@gmail.com>
2016-10-01 01:19:46 +0200

    Added Favorite Artists and Albums

---
+++ TidalExplorer/Controllers/TidalUserFavoritesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using TidalExplorer.TidalIntegration;
+
+namespace TidalExplorer.Controllers
+{
+    public class TidalUserFavoritesController : Controller
+    {
+        [Authorize]
+        public async Task<ActionResult> Artists()
+        {
+            var session = await OpenTidlIntegrator.RestoreSessionFromClaimsIdentity(User.Identity);
+
+            if (session == null)
+                return new EmptyResult();
+
+            var favoriteArtists = await session.GetFavoriteArtists();
+
+            if (favoriteArtists == null)
+                return new EmptyResult();
+
+            return View("Artists", favoriteArtists);
+        }
+        
+
+        [Authorize]
+        public async Task<ActionResult> Albums()
+        {
+            var session = await OpenTidlIntegrator.RestoreSessionFromClaimsIdentity(User.Identity);
+
+            if (session == null)
+                return new EmptyResult();
+
+            var favoriteAlbums = await session.GetFavoriteAlbums();
+
+            if (favoriteAlbums == null)
+                return new EmptyResult();
+
+            return View("Albums", favoriteAlbums);
+        }
+    }
+}
